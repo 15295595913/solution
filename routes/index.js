e7867de67ef5bf7127df808d279f1a04a5bf0133
15295595913 <15295595913@163.com>
2017-05-27 18:01:51 +0800

    Signed-off-by: 15295595913 <15295595913@163.com>

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,45 @@
 var express = require('express');
 var session = require('express-session');
-
+// var RedisStore = require('connect-redis')(session);
 var request = require('request');
 
 var router = express.Router();
 
+var app = express();
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 // res.render('index', { title: 'Express1',indexO:{"id": 1} });
 
 
 // });
+app.use(session({
+    secret: 'hubwiz app', //secret的值建议使用随机字符串
+    cookie: {maxAge: 60 * 1000 * 30} ,// 过期时间（毫秒）
+    name: 'hch'
+}));
+
 
 router.get('/', function (req, res, next) {
-    console.log('================start 拦截器 ====');
-    console.log('拦截器');
-    console.log('===============end=====');
-    express.use(session({
-        name: 'hch',
-        psw: '123'
-    }));
-    console.log(req.session.name);
+
+    res.send('hello, session id:' + res.session + ' count:');
+    // console.log(res.session);
     //数据
-    var data = {
-        title: 'Map',
-        time: (new Date).toString(),
-        list: [
-            {
-                id: '1',
-                name: '张三'
-            },
-            {
-                id: '2',
-                name: '李四'
-            }
-        ]
-    };
-    //渲染模板
-    res.render('index/index', data);
+    // var data = {
+    //     title: 'Map',
+    //     time: (new Date).toString(),
+    //     list: [
+    //         {
+    //             id: '1',
+    //             name: '张三'
+    //         },
+    //         {
+    //             id: '2',
+    //             name: '李四'
+    //         }
+    //     ]
+    // };
+    // //渲染模板
+    // res.render('index/index', data);
 });
 
 module.exports = router;
